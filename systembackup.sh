e06f7040b7e6c98e198d227901503958656167cd
Owen Harvey <owenlharvey@gmail.com>
2015-02-07 01:32:31 +1000

    Initial commit. Script to incrementally backup /etc and /home daily using hardlinks as well as keeping a list of installed packages.

---
+++ systembackup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#Incremental Backup Script
+ 
+# Current date, ISO-8601 format
+TODAY=`date -I`
+
+# Last backup date
+LASTBACKUP=`ls /run/media/owen/95920304-fb6c-4a82-a0d5-05021b03f124/Backup/ | tail -n 1`
+ 
+# Source directory
+SRC="/home/owen /etc"
+ 
+# Target directory
+TRG="/run/media/owen/95920304-fb6c-4a82-a0d5-05021b03f124/Backup/$TODAY"
+ 
+# Link directory
+LNK="/run/media/owen/95920304-fb6c-4a82-a0d5-05021b03f124/Backup/$LASTBACKUP"
+ 
+# Rsync options
+# Archive, hardlink, delete
+OPT="-ah --delete --link-dest=$LNK"
+ 
+# Run backup
+rsync $OPT $SRC $TRG
+
+# Keep a list of installed packages.
+# If package list already exists, recreate it.
+# Allows script to be run manually without error.
+# Native packages.
+if [ -e $TRG/pkglist.txt ]
+then
+	rm $TRG/pkglist.txt
+fi
+pacman -Qqen > $TRG/pkglist.txt
+
+# AUR packages
+if [ -e $TRG/pkglist_aur.txt ]
+then
+	rm $TRG/pkglist_aur.txt
+fi
+pacman -Qqem > $TRG/pkglist_aur.txt
