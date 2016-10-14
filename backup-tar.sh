9dc9d4de9795356f35def3f814b1ac7d844c4506
Owen Harvey <owenlharvey@gmail.com>
2016-10-15 00:22:01 +1000

    Script to make tar-balls

---
+++ backup-tar.sh
@@ -0,0 +1,6 @@
+#/usr/bin/bash
+source /etc/backup/systembackup.conf
+for i in $@
+do
+  tar -c "$i" | openssl enc -e "$CIPHER" -pass "$PASSWORD" | tee "$i.tar.enc$CIPHER" | openssl dgst "$HMAC_ALGO" -hmac "$HMAC_KEY" -r | cut -f 1 -d " " > "$i.tar.hmac$HMAC_ALGO"
+done
