d47a7c969f0a4a46881a69d06e8201f072d48d6a
Owen Harvey <owenlharvey@gmail.com>
2017-04-04 10:14:24 +1000

    Pulled some of the code into functions

--- systembackup.sh
+++ systembackup.sh
@@ -60,20 +60,19 @@ do
 done
 
 processFile () {
-  name=$1
-  LAST_NAME="$LOCAL_LAST_BACKUP/$name"
+  FILE_PATH=$1
+  LAST_NAME="$LOCAL_LAST_BACKUP/$FILE_PATH"
   LAST_HMAC="$LAST_NAME.hmac$HMAC_ALGO"
   LAST_ENC="$LAST_NAME.enc$CIPHER"
-  TARGET_NAME="$LOCAL_BACKUP_TARGET$name"
+  TARGET_NAME="$LOCAL_BACKUP_TARGET$FILE_PATH"
   TARGET_HMAC="$TARGET_NAME.hmac$HMAC_ALGO"
   TARGET_ENC="$TARGET_NAME.enc$CIPHER"
+  encrypt () {
+    openssl enc -e "$CIPHER" -pass "$PASSWORD" -in "$FILE_PATH" | tee "$TARGET_ENC" | openssl dgst "$HMAC_ALGO" -hmac "$HMAC_KEY" -r | cut -f 1 -d " " > "$TARGET_HMAC" 
+  }
   if [ -f "$LAST_ENC" ] && [ -f "$LAST_HMAC" ]
   then
     cmp_status=1
-    #echo "decrypt backup file and compare with current file"
-    # I remembered that io redirection as a file is totally a thing.
-    # Should also be nice on memory usage too!
- 
     # Check that the hmac is valid, THEN check if the crypto needs to be done.
     if cmp -s "$LAST_HMAC" <(openssl dgst "$HMAC_ALGO" -hmac "$HMAC_KEY" -r < "$LAST_ENC" | cut -f 1 -d " ")
     then
@@ -81,11 +80,9 @@ processFile () {
       SALT_IV=$(openssl enc -d "$CIPHER" -pass "$PASSWORD" -P -in "$LAST_ENC" | tr "\n" " ")
       SALT=$(echo "${SALT_IV:?}" | cut -d " " -f 1 | cut -d '=' -f 2)
       IV=$(echo "$SALT_IV" | cut -d " " -f 4 | cut -d '=' -f 2)
-      if cmp -s "$LAST_ENC" <(openssl enc -e "$CIPHER" -pass "$PASSWORD" -S "$SALT" -iv "$IV" -in "$name")
+      if cmp -s "$LAST_ENC" <(openssl enc -e "$CIPHER" -pass "$PASSWORD" -S "$SALT" -iv "$IV" -in "$FILE_PATH")
       then
         cmp_status=0
-      else
-        cmp_status=1
       fi
     fi
     if [ $cmp_status -eq 0 ]
@@ -96,10 +93,10 @@ processFile () {
     else
       # Files are differnet
       # Write encrypted file to disk and generate hmac
-      openssl enc -e "$CIPHER" -pass "$PASSWORD" -in "$j" | tee "$TARGET_ENC" | openssl dgst "$HMAC_ALGO" -hmac "$HMAC_KEY" -r | cut -f 1 -d " " > "$TARGET_HMAC" 
+      encrypt
     fi
   else
-    openssl enc -e "$CIPHER" -pass "$PASSWORD" -in "$j" | tee "$TARGET_ENC" | openssl dgst "$HMAC_ALGO" -hmac "$HMAC_KEY" -r | cut -f 1 -d " " > "$TARGET_HMAC"
+    encrypt
   fi
 }
 
@@ -107,7 +104,10 @@ echo "Start local backup"
 for i in $SOURCES
 do
   IFS=$'\n'
-  find "$i" -type f -exec processFile {} \;
+  for j in $(find "$i" -type f)
+  do
+    processFile "$j"
+  done
   unset IFS
 done
 
@@ -115,26 +115,30 @@ done
 # If package list already exists, recreate it.
 # Allows script to be run manually without error.
 # Native packages.
-if [ -e "$LOCAL_BACKUP_TARGET/pkglist.txt.enc$CIPHER" ]
+PKG_ENC="$LOCAL_BACKUP_TARGET/pkglist.txt.enc$CIPHER"
+if [ -e "$PKG_ENC" ]
 then
-  rm "$LOCAL_BACKUP_TARGET/pkglist.txt.enc$CIPHER"
+  rm "$PKG_ENC" 
 fi
-if [ -e "$LOCAL_BACKUP_TARGET/pkglist.txt.hmac$HMAC_ALGO" ]
+PKG_HMAC="$LOCAL_BACKUP_TARGET/pkglist.txt.hmac$HMAC_ALGO"
+if [ -e "$PKG_HMAC" ]
 then
   rm "$LOCAL_BACKUP_TARGET/pkglist.txt.hmac$HMAC_ALGO"
 fi
-pacman -Qqen | openssl enc -e "$CIPHER" -pass "$PASSWORD" | tee "$LOCAL_BACKUP_TARGET/pkglist.txt.enc$CIPHER" | openssl dgst "$HMAC_ALGO" -hmac "$HMAC_KEY" -r | cut -f 1 -d " " > "$LOCAL_BACKUP_TARGET/pkglist.txt.hmac$HMAC_ALGO"
+pacman -Qqen | openssl enc -e "$CIPHER" -pass "$PASSWORD" | tee "$PKG_ENC" | openssl dgst "$HMAC_ALGO" -hmac "$HMAC_KEY" -r | cut -f 1 -d " " > "$PKG_HMAC"
 
 # AUR packages
-if [ -e "$LOCAL_BACKUP_TARGET/pkglist_aur.txt.enc$CIPHER" ]
+AUR_ENC="$LOCAL_BACKUP_TARGET/pkglist_aur.txt.enc$CIPHER"
+if [ -e "$AUR_ENC" ]
 then
-  rm "$LOCAL_BACKUP_TARGET/pkglist_aur.txt.enc$CIPHER"
+  rm "$AUR_ENC"
 fi
-if [ -e "$LOCAL_BACKUP_TARGET/pkglist_aur.txt.hmac$HMAC_ALGO" ]
+AUR_HMAC="$LOCAL_BACKUP_TARGET/pkglist_aur.txt.hmac$HMAC_ALGO"
+if [ -e "$AUR_HMAC" ]
 then
-  rm "$LOCAL_BACKUP_TARGET/pkglist_aur.txt.hmac$HMAC_ALGO"
+  rm "$AUR_HMAC"
 fi
-pacman -Qqem | openssl enc -e "$CIPHER" -pass "$PASSWORD" | tee "$LOCAL_BACKUP_TARGET/pkglist_aur.txt.enc$CIPHER" | openssl dgst "$HMAC_ALGO" -hmac "$HMAC_KEY" -r | cut -f 1 -d " " > "$LOCAL_BACKUP_TARGET/pkglist_aur.txt.hmac$HMAC_ALGO"
+pacman -Qqem | openssl enc -e "$CIPHER" -pass "$PASSWORD" | tee "$AUR_ENC" | openssl dgst "$HMAC_ALGO" -hmac "$HMAC_KEY" -r | cut -f 1 -d " " > "$AUR_HMAC"
 
 # Only keep a limited number of backups
 while [ "$(ls "$LOCAL_BACKUP_DIRECTORY" | wc -l)" -gt "$LOCAL_BACKUPS_TO_KEEP" ]
@@ -160,6 +164,7 @@ do
 
   REMOTE_LAST_BACKUP=$(ssh -xo "BatchMode yes" -i "$REMOTE_SSH_ID" "$REMOTE_USER@$REMOTE_SERVER" ls "$REMOTE_BACKUP_DIRECTORY" | tail -n 1)
   REMOTE_BACKUP_TARGET="$REMOTE_BACKUP_DIRECTORY/$CURRENT_TIME"
+  REMOTE_BACKUP_DIRECTORY_LAST="$REMOTE_BACKUP_DIRECTORY/$REMOTE_LAST_BACKUP"
   USER_SERVER="$REMOTE_USER@$REMOTE_SERVER"
   USER_SERVER_DIRECTORY="$USER_SERVER:$REMOTE_BACKUP_DIRECTORY"
   USER_SERVER_DIRECTORY_LAST="$USER_SERVER_DIRECTORY/$REMOTE_LAST_BACKUP"
@@ -167,8 +172,8 @@ do
   # Check if we need to do a full backup, or an update
   if [ "$REMOTE_LAST_BACKUP" != "$CURRENT_TIME"  ]
   then
-    echo "remote directory cp --reflink=auto -rp $REMOTE_BACKUP_DIRECTORY/$REMOTE_LAST_BACKUP $REMOTE_BACKUP_TARGET"
-    ssh -xo "BatchMode yes" -i "$REMOTE_SSH_ID" "$USER_SERVER" sudo cp --reflink=auto -rp "$REMOTE_BACKUP_DIRECTORY_LAST $REMOTE_BACKUP_TARGET"
+    echo "remote directory cp --reflink=auto -rp $REMOTE_BACKUP_DIRECTORY_LAST $REMOTE_BACKUP_TARGET"
+    ssh -xo "BatchMode yes" -i "$REMOTE_SSH_ID" "$USER_SERVER" sudo cp --reflink=auto -rp "$REMOTE_BACKUP_DIRECTORY_LAST" "$REMOTE_BACKUP_TARGET"
     echo "remote rsync -ze ssh -i $REMOTE_SSH_ID $RSYNC_OPTIONS --inplace $LOCAL_BACKUP_TARGET $USER_SERVER_DIRECTORY"
     rsync -ze "ssh -xi $REMOTE_SSH_ID" $RSYNC_OPTIONS --inplace "$LOCAL_BACKUP_TARGET" "$USER_SERVER_DIRECTORY"
   else
